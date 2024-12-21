7fc8fcf7664b32a2de5b4dd337af8e0b3b914bee
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-21 15:57:17 -0300

    Capitulo 2 / parte 2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req,res) => {
   const { method, url } = req
   if(method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários');
+    return res.setHeader('Content-type','application/json').end(JSON.stringify(users),);
   }
   if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: "John Smith",
+      email: "john.smith@example.com"
+    })
     return res.end('Cadastro de usuário');
   }
 
