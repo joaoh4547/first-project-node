cce2c29a323a11da6c55a590cb9b73332c7fc9a3
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-21 16:55:02 -0300

    Capitulo 3 / parte 2

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,6 @@
 //process.stdin.pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -19,4 +19,22 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+class InverseNumberStream extends Transform{
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    const buf = Buffer.from(String(transformed))
+    callback(null, buf);
+  }
+}
+
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTenStream());
