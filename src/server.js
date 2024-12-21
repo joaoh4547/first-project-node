ef1bfadc5d14468a44d92f75dc324e0c822be869
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-21 13:23:35 -0300

    capitulo 2 / parte 1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import http from 'node:http'
 
 const server = http.createServer((req,res) => {
+  const { method, url } = req
+  if(method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários');
+  }
+  if(method === 'POST' && url === '/users') {
+    return res.end('Cadastro de usuário');
+  }
+
+
   return res.end('Hello world!');
 })
 
