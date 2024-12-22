b38d0507fef6f730942feab18a0559802b4873a2
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-22 17:09:20 -0300

    Capitulo 4 parte 1

--- src/server.js
+++ src/server.js
@@ -1,25 +1,13 @@
 import http from 'node:http'
-import {randomUUID} from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
-const database = new Database()
-const server = http.createServer(async (req,res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
   await json(req, res)
-  if(method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    return res.end(JSON.stringify(users));
-  }
-  if(method === 'POST' && url === '/users') {
-    const {name, email} = req.body
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-    database.insert('users',user)
-    return res.writeHead(201).end();
+  const route = routes.find(r => r.method === method && r.path === url)
+  if (route) {
+    return route.handler(req, res)
   }
   return res.writeHead(404).end();
 })
